6fb164298a9cd0bd6826d4b44ed43840613b70b6
Jcasanova1990 <jeremykcasanova@gmail.com>
2023-12-22 12:23:09 -0500

    <Pass It Around Application Created>

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -1,8 +1,31 @@
-const express = require("express")
+const express = require('express')
 const app = express()
+const port = 3001
 
+app.get('/', (req, res) => {
+    res.send('<h1>99 Bottles of beer on the wall</h1><a href="/98">Take one down, pass it around</a>')
+});
 
+app.get('/:number_of_bottles', (req, res) => {
+    try {
+        const numberOfBottles = parseInt(req.params.number_of_bottles, 10)
 
-app.listen(3001, () => {
-    console.log("mmmmmmmmm")
+        if (isNaN(numberOfBottles)) {
+            throw new Error('1 less bottle')
+        }
+
+        if (numberOfBottles === 0) {
+            res.send('<h1>No more bottles of beer on the wall</h1><a href="/">Start Over</a>')
+        } else {
+            const nextNumber = numberOfBottles - 1
+            res.send(`<h1>${numberOfBottles} Bottles of beer on the wall</h1><a href="/${nextNumber}">Take one down, pass it around</a><br><a href="/">Start Over</a>`)
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).send('Bad Request')
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at port, ${port}`)
 })
